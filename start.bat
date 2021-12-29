9af0f7c8776d3ce9e9b548b9fa2912f093c6ded3
MrDiamond64 <49098391+MrDiamond64@users.noreply.github.com>
2021-12-28 20:00:46 -0600

    b

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-title Realm Code Generator - By MrDiamond64 ^| Total Attempts: 0
+title Discord Token Cracker - By MrDiamond64 ^| Total Attempts: 0
 if not exist node_modules\ cmd /c npm i
 echo {}>invalidtokens.json
 node main.js
